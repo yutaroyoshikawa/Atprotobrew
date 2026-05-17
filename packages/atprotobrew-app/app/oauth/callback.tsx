49ebea2ab5e64a3f04746527f6f965f041564609
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2026-05-17 17:49:25 +0900

    feat: native 用 Atpassport リダイレクト先ページの用意

--- packages/atprotobrew-app/app/oauth/callback.tsx
+++ packages/atprotobrew-app/app/oauth/callback.tsx
@@ -2,13 +2,17 @@ import { ActivityIndicator } from "react-native";
 import { useRouter } from "expo-router";
 import { AppVStack } from "@atprotobrew/common/core/components/AppVStack";
 import { useEffect } from "react";
+import { useAuthContext } from "../../modules/auth/AuthProvider";
 
 function OAuthCallback() {
   const router = useRouter();
+  const { authState } = useAuthContext();
 
   useEffect(() => {
-    router.dismissAll();
-  });
+    if (authState.status === "authenticated") {
+      router.dismissAll();
+    }
+  }, [authState.status]);
 
   return (
     <AppVStack>
