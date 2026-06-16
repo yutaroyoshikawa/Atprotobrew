ac9e8850064a8f39f4c7f0f3711fc68c8d0eb944
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2026-06-16 17:20:59 +0900

    fix: format

--- packages/atprotobrew-app/app/(app)/store/index.tsx
+++ packages/atprotobrew-app/app/(app)/store/index.tsx
@@ -8,49 +8,49 @@ import { View } from "react-native";
 import { useAuthContext } from "../../../modules/auth/AuthProvider";
 
 export default function Screen() {
-	const { authState } = useAuthContext();
+  const { authState } = useAuthContext();
 
-	if (authState.status !== "authenticated") {
-		return null;
-	}
+  if (authState.status !== "authenticated") {
+    return null;
+  }
 
-	return (
-		<Suspense>
-			<StoreScreenContent session={authState.session} />
-		</Suspense>
-	);
+  return (
+    <Suspense>
+      <StoreScreenContent session={authState.session} />
+    </Suspense>
+  );
 }
 
 interface StoreScreenContentProps {
-	session: OAuthSession;
+  session: OAuthSession;
 }
 
 function StoreScreenContent({ session }: StoreScreenContentProps) {
-	const { data } = useFetchLaunchers({ agent: session });
+  const { data } = useFetchLaunchers({ agent: session });
 
-	return (
-		<View style={[a.h_full]}>
-			<View style={[a.flex_col]}>
-				<View style={[a.items_center]}>
-					<AppH1>Atprotobrew store</AppH1>
-				</View>
+  return (
+    <View style={[a.h_full]}>
+      <View style={[a.flex_col]}>
+        <View style={[a.items_center]}>
+          <AppH1>Atprotobrew store</AppH1>
+        </View>
 
-				<View style={[a.flex_1, a.w_full, a.px_4, a.py_4]}>
-					<View style={[a.flex_col, a.gap_6]}>
-						{data.body.items.map((channel) => {
-							return (
-								<StoreItem
-									key={channel.title}
-									title={channel.title}
-									thumbnail={channel.thumbnail}
-									author={channel.author}
-									description={channel.description}
-								/>
-							);
-						})}
-					</View>
-				</View>
-			</View>
-		</View>
-	);
+        <View style={[a.flex_1, a.w_full, a.px_4, a.py_4]}>
+          <View style={[a.flex_col, a.gap_6]}>
+            {data.body.items.map((channel) => {
+              return (
+                <StoreItem
+                  key={channel.title}
+                  title={channel.title}
+                  thumbnail={channel.thumbnail}
+                  author={channel.author}
+                  description={channel.description}
+                />
+              );
+            })}
+          </View>
+        </View>
+      </View>
+    </View>
+  );
 }
